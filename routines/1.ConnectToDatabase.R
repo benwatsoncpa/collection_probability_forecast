cffec06b16bb2485db43f0efc93341842c56f68d
Ben Watson <ben.watson.ca@gmail.com>
2019-02-18 17:17:40 -0500

    Initial commit

--- routines/1.ConnectToDatabase.R
+++ routines/1.ConnectToDatabase.R
@@ -1,6 +1,6 @@
 # 1.0 Connect to the ARRMS Database using RODBC -------------------------------
 
-arrm<-odbcConnect("ARRM64",
-                  uid="arrm_frms_ro",
-                  pwd="ntU7n_P",
+arrm<-odbcConnect("",
+                  uid="",
+                  pwd="",
                   believeNRows=F)
